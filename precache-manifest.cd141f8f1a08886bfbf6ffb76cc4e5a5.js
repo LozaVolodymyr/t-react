a13b5b377a3079a921fca801e4d4734bf92ad455
Loza <vloza@paypal.com>
2019-12-03 16:10:07 -0800

    dist to static

---
+++ precache-manifest.cd141f8f1a08886bfbf6ffb76cc4e5a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ee49d9ff6254ceb9aeea34a8553e32",
+    "url": "/pages/vloza/pph-react-sample-app/index.html"
+  },
+  {
+    "revision": "64611d2996b42ec086d4",
+    "url": "/pages/vloza/pph-react-sample-app/static/css/2.665f3fd3.chunk.css"
+  },
+  {
+    "revision": "e0627a793d1651fe239b",
+    "url": "/pages/vloza/pph-react-sample-app/static/css/main.ddd0aaef.chunk.css"
+  },
+  {
+    "revision": "64611d2996b42ec086d4",
+    "url": "/pages/vloza/pph-react-sample-app/static/js/2.5254609f.chunk.js"
+  },
+  {
+    "revision": "e0627a793d1651fe239b",
+    "url": "/pages/vloza/pph-react-sample-app/static/js/main.487b58a5.chunk.js"
+  },
+  {
+    "revision": "efb2436330339e0ca322",
+    "url": "/pages/vloza/pph-react-sample-app/static/js/runtime-main.90e99b01.js"
+  }
+]);
